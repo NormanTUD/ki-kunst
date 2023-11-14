1ab7c74ef9912744fb23cfcc87ff681edf20a609
Norman Koch <norman.koch@tu-dresden.de>
2023-11-14 11:07:19 +0100

    initial

--- api.php
+++ api.php
@@ -5,18 +5,18 @@ $description = $_POST['description'];
 $description = preg_replace("/[^a-zA-ZäÄöÖüÜß ]/", " ");
 
 try {
-    // Übergeben Sie die Beschreibung an die Shell-Ausführung und speichern Sie die Ausgabe in $output
-    $output = shell_exec("bash api.sh " . escapeshellarg($description));
+	// Übergeben Sie die Beschreibung an die Shell-Ausführung und speichern Sie die Ausgabe in $output
+	$output = shell_exec("bash api.sh " . escapeshellarg($description));
 
-    if ($output !== null) {
-        // Senden Sie die Ausgabe zurück an JavaScript
-        echo $output;
-    } else {
-        // Keine Ausgabe erhalten
-        echo "Keine Ausgabe von api.sh erhalten.";
-    }
+	if ($output !== null) {
+		// Senden Sie die Ausgabe zurück an JavaScript
+		echo $output;
+	} else {
+		// Keine Ausgabe erhalten
+		echo "Keine Ausgabe von api.sh erhalten.";
+	}
 } catch (Exception $e) {
-    // Fehler beim Aufrufen der api.sh oder anderen Ausnahmen
-    echo "Fehler aufgetreten: " . $e->getMessage();
+	// Fehler beim Aufrufen der api.sh oder anderen Ausnahmen
+	echo "Fehler aufgetreten: " . $e->getMessage();
 }
 ?>
