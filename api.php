d393f919c1fd927b3dc08641343b0fe0a89521e0
Norman Koch <norman.koch@tu-dresden.de>
2023-11-14 10:56:07 +0100

    initial

---
+++ api.php
@@ -0,0 +1,20 @@
+<?php
+// Erhalten Sie die übergebene Beschreibung von JavaScript
+$description = $_POST['description'];
+
+try {
+    // Übergeben Sie die Beschreibung an die Shell-Ausführung und speichern Sie die Ausgabe in $output
+    $output = shell_exec("bash api.sh " . escapeshellarg($description));
+
+    if ($output !== null) {
+        // Senden Sie die Ausgabe zurück an JavaScript
+        echo $output;
+    } else {
+        // Keine Ausgabe erhalten
+        echo "Keine Ausgabe von api.sh erhalten.";
+    }
+} catch (Exception $e) {
+    // Fehler beim Aufrufen der api.sh oder anderen Ausnahmen
+    echo "Fehler aufgetreten: " . $e->getMessage();
+}
+?>
---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ausstellung</title>
+    <script src="jquery.js"></script>
+</head>
+<body>
+	<style>
+		<?php include("style.css"); ?>
+	</style>
+
+    <input style="width: 80%" id="description" value="" placeholder="Beschreibe hier, was chatGPT malen soll" /> <button onclick='call_api()'>Malen!</button>
+
+    <div id="history"></div>
+    <div id="response"></div>
+    <script>
+var response_counter = 0;
+var global_r;
+        function call_api() {
+		response_counter++;
+            var description = $("#description").val();
+            try {
+                // Clear the input field
+		    $("#description").val('');
+		    $("#description").attr("disabled", true);
+
+                // Display loading animation while waiting for the response
+                var loadingDiv = '<div><img src="loading.gif" alt="Loading"></div>';
+
+	    var responseDiv = $(
+		'<div id="response_' + response_counter + '_sent" class="message sent">' + description + '</div>' +
+		'<div id="response_' + response_counter + '_received" class="message received">' + response + '</div>'
+	    );
+
+		var ok = 0;
+
+	    $("#history").prepend(responseDiv);
+
+                $("#response_" + response_counter + "_received").html(loadingDiv);
+
+                var ajaxRequest = $.ajax({
+                    type: "POST",
+                    url: "api.php",
+                    data: { description: description },
+                    success: function (response) {
+                        try {
+                            // Replace newline characters with <br> in the response
+				var r = response.replace(/\\n/g, "\n");
+				r = r.replace("</svg>", "</svg><pre style='white-space: pre-wrap'>");
+				r = r + "</pre>";
+
+                            // Create a new div for each question and answer and append it to the history
+			    ok++;
+				console.log(r);
+				global_r = r;
+			    $("#response_" + response_counter + "_received").html(r);
+			    $("#description").attr("disabled", false).focus();
+                        } catch (error) {
+                            console.warn("Fehler beim Hinzufügen zur History: " + error.message);
+			    $("#description").attr("disabled", false).focus();
+                        }
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        console.warn("A: Fehler beim API-Aufruf: " + errorThrown);
+		        $("#description").attr("disabled", false).focus();
+                    }
+                });
+
+                // Set a timeout to handle cases where the API call takes too long
+                setTimeout(function () {
+			if(!ok) {
+			    ajaxRequest.abort(); // Abort the API call on timeout
+			    $("#response_" + response_counter + "_received").html('Fehler: Timeout').css('color', 'red');
+			}
+		        $("#description").attr("disabled", false).focus();
+                }, 2*60000); // 2*60 seconds
+            } catch (error) {
+                console.error("B: Fehler beim API-Aufruf: " + error.message);
+		        $("#description").attr("disabled", false).focus();
+            }
+        }
+$( document ).ready(function() {
+	// Execute a function when the user presses a key on the keyboard
+	$("#description")[0].addEventListener("keypress", function(event) {
+		// If the user presses the "Enter" key on the keyboard
+		if (event.key === "Enter") {
+			// Cancel the default action, if needed
+			event.preventDefault();
+			// Trigger the button element with a click
+			call_api();
+		}
+	});
+
+	$("#description").focus();
+});
+    </script>
+</body>
+</html>
+
