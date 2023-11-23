d1c1915c6167b2906b9fd0f97ef9f63756073772
Norman Koch <norman.koch@tu-dresden.de>
2023-11-23 11:29:35 +0100

    initial

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
 	<head>
 		<title>Ausstellung</title>
 		<script src="jquery.js"></script>
-		<script src="html2canvas.min.js"></script>
 		<style>
 			<?php include("style.css"); ?>
 			<?php include("animate.css"); ?>
