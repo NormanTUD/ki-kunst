9a92cd116e4d99f16e8a2dc578aa940dfb21faf0
Norman Koch <norman.koch@tu-dresden.de>
2023-11-23 10:26:18 +0100

    initial

--- index.php
+++ index.php
@@ -235,6 +235,21 @@
 			}
 
 			document.body.addEventListener('keypress', type);
+
+			var time = new Date().getTime();
+			$(document.body).bind("keypress", function(e) {
+				time = new Date().getTime();
+			});
+
+			function refresh() {
+				if(new Date().getTime() - time >= (5 * 60000)) {
+					window.location.reload(true);
+				} else {
+					setTimeout(refresh, 10000);
+				}
+			}
+
+			setTimeout(refresh, 10000);
 		</script>
 	</body>
 </html>
